f9eb102a2561e167f27e5862d8e0dcd851912794
GyoungTaeKim <kim3jjj@naver.com>
2024-06-26 16:45:06 +0900

    [김경태] + 아이콘 / 색상 변경 및 텍스트 사이즈 변경

--- lib/presentation/pages/meet/widgets/empty_meet_widget.dart
+++ lib/presentation/pages/meet/widgets/empty_meet_widget.dart
@@ -9,16 +9,22 @@ class EmptyMeetWidget extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconButton(onPressed: () {
-          print('약속잡기');
-        }, icon: Icon(Icons.add)),
+        IconButton(
+            onPressed: () {
+              print('약속잡기');
+            },
+            icon: Icon(
+              Icons.add_circle_sharp,
+              color: Colors.amberAccent,
+              size: 40,
+            )),
         SizedBox(
           height: 10,
         ),
         Center(
           child: Text(
             '정해진 약속이 없습니다\n버튼을 입력해서 약속을 정해봐요!',
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 25),
           ),
         ),
       ],
