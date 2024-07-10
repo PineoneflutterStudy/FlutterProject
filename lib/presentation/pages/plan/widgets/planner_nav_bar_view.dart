771c0acd378f4edd2099356b7e21920248d35eb7
Nanioi <0802skdus@naver.com>
2024-07-10 15:08:11 +0900

    [곽나연][KKULTRIP-18] 공통 에러 팝업 위치 이동

--- lib/presentation/pages/plan/widgets/planner_nav_bar_view.dart
+++ lib/presentation/pages/plan/widgets/planner_nav_bar_view.dart
@@ -4,21 +4,21 @@ import '../../../../core/utils/constant.dart';
 import '../../../../domain/model/display/category/category.model.dart';
 
 class PlannerNavBarView extends StatelessWidget {
-  const PlannerNavBarView(this.menuType, this.categorys, {super.key});
+  const PlannerNavBarView(this.menuType, this.categoryList, {super.key});
 
   final MenuType menuType;
-  final List<Category> categorys;
+  final List<Category> categoryList;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: TabBarView(
         children: List.generate(
-          categorys.length,
+          categoryList.length,
           (index) => Column(
             children: [
               Text('menuType : ${menuType}'),
-              Text('menuType : ${menuType.name}'),
+              Text('menuType : ${categoryList[index].ctgrName}'),
             ],
           ),
         ),
