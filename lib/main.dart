83a29757ecd93e2528789f4d97f6a17b02ca160c
Eogeum <Eogeum@naver.com>
2024-07-15 16:21:29 +0900

    [최정창][KKULTRIP-30] 카카오 로그인 SDK 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_project_team1/service_locator.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+
 import 'core/theme/theme_data.dart';
 import 'presentation/routes/routes.dart';
+import 'service_locator.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: '.env');
   AuthRepository.initialize(appKey: dotenv.env['KAKAO_JAVA_SCRIPT_KEY'] ?? '');
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
 
   setLocator();
   runApp(const MainApp());
