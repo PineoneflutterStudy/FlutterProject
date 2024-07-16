3102f8ba01a717f5e38f8d537c0e2ed26d08ed65
Eogeum <Eogeum@naver.com>
2024-07-16 16:57:44 +0900

    [최정창][KKULTRIP-30] 카카오톡 로그인 및 카카오 계정 로그인 구현

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:logger/logger.dart';
 
 import '../../../core/utils/logger.dart';
@@ -72,7 +74,8 @@ class _LoginPageState extends State<LoginPage> {
           '${authType.name} 로그인',
           style: TextStyle(fontSize: 36, color: authType.fontColor),
         ),
-        style: ButtonStyle(backgroundColor: WidgetStateProperty.all(authType.bgColor)),
+        style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(authType.bgColor)),
         onPressed: () => onPressedAuthListItem(authType),
       );
 
@@ -99,7 +102,36 @@ class _LoginPageState extends State<LoginPage> {
     _logger.i('$_tag launchNaverLogin()');
   }
 
-  void launchKakaoLogin() {
-    _logger.i('$_tag launchKakaoLogin()');
+  Future<void> launchKakaoLogin() async {
+    // 카카오톡 실행 가능 여부 확인
+    // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        _logger.i('$_tag KakaoTalk Login Successed');
+      } catch (error) {
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          _logger.i('$_tag KakaoTalk Login Canceled');
+          return;
+        }
+
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        _logger.e('$_tag KakaoTalk Login Failed, error = $error');
+        await loginWithKakaoAccount();
+      }
+    } else {
+      await loginWithKakaoAccount();
+    }
+  }
+
+  Future<void> loginWithKakaoAccount() async {
+    try {
+      await UserApi.instance.loginWithKakaoAccount();
+      _logger.i('$_tag KakaoAccount Login Successed');
+    } catch (error) {
+      _logger.e('$_tag KakaoAccount Login Failed, error = $error');
+    }
   }
 }
