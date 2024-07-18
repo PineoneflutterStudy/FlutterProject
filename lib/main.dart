43aed822d676a3a5b08ae6672d8ea80b09078579
Eogeum <Eogeum@naver.com>
2024-07-18 14:42:40 +0900

    [최정창][KKULTRIP-30] env FlutterConfig 적용 및 카카오 계정 로그인 구현 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
@@ -11,9 +11,10 @@ void main() async {
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: '.env');
-  AuthRepository.initialize(appKey: dotenv.env['KAKAO_JAVA_SCRIPT_KEY'] ?? '');
-  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
+  // FlutterConfig.loadValueForTesting({'KAKAO_NATIVE_APP_KEY': ''}); // 테스트 필요할 때 사용
+  await FlutterConfig.loadEnvVariables();
+  AuthRepository.initialize(appKey: FlutterConfig.get('KAKAO_JAVA_SCRIPT_KEY') ?? '');
+  KakaoSdk.init(nativeAppKey: FlutterConfig.get('KAKAO_NATIVE_APP_KEY'));
 
   setLocator();
   runApp(const MainApp());
