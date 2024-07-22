d2e7fc749b5e98eae597b0b1113732f709171d95
Eogeum <Eogeum@naver.com>
2024-07-22 10:58:53 +0900

    [최정창][KKULTRIP-30] 카카오 로그인 토근 로그 추가

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -126,8 +126,11 @@ class _LoginPageState extends State<LoginPage> {
     // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
     if (await isKakaoTalkInstalled()) {
       try {
-        await UserApi.instance.loginWithKakaoTalk();
+        OAuthToken result = await UserApi.instance.loginWithKakaoTalk();
+        _accessToken = result.accessToken;
         _logger.i('$_tag KakaoTalk Login Successed');
+        _logger.d('$_tag result = $result');
+        _logger.d('$_tag accessToken = $_accessToken');
       } catch (error) {
         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
@@ -147,8 +150,11 @@ class _LoginPageState extends State<LoginPage> {
 
   Future<void> loginWithKakaoAccount() async {
     try {
-      await UserApi.instance.loginWithKakaoAccount();
+      OAuthToken result = await UserApi.instance.loginWithKakaoAccount();
+      _accessToken = result.accessToken;
       _logger.i('$_tag KakaoAccount Login Successed');
+      _logger.d('$_tag result = $result');
+      _logger.d('$_tag accessToken = $_accessToken');
     } catch (error) {
       _logger.e('$_tag KakaoAccount Login Failed, error = $error');
     }
