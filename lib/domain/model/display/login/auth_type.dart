d4ceab237be13c0d507c0ad902820cc8a7b486f9
Eogeum <Eogeum@naver.com>
2024-07-29 12:56:59 +0900

    [최정창][KKULTRIP-30] 로그인 인증 타입 추가(이메일)

--- lib/domain/model/display/login/auth_type.dart
+++ lib/domain/model/display/login/auth_type.dart
@@ -2,31 +2,23 @@ import 'dart:ui';
 
 import '../../../../core/theme/constant/app_icons.dart';
 
-/**
- * 로그인 화면에서 제공하는 인증 타입
- *
- * [code] = 제공사 영어 이름 맨 앞자리
- * [name] = 제공사 한국어 이름
- * [iconPath] = 제공사 로고 아이콘
- * [bgColor] = 제공사 로그인 버튼 배경 색
- * [fontColor] = 제공사 로그인 버튼 폰트 색
- *
- * @author Eogeum@naver.com
- * @since 2024-07-03
- */
-enum AuthType {
-  google('G', '구글', AppIcons.imgLoginLogoGoogle, Color(0xFFFFFFFF),
-      Color(0xFF000000)),
-  naver('N', '네이버', AppIcons.imgLoginLogoNaver, Color(0xFF03C75A),
-      Color(0xFFFFFFFF)),
-  kakao('K', '카카오', AppIcons.imgLoginLogoKakao, Color(0xFFFEE500),
-      Color(0xDB000000)),
-  // email('E', '이메일', AppIcons.imgLoginEmail, Color(0xFFFFFFFF),
-  //     Color(0xFFFFFFFF))
-  ;
+/// ## 로그인 화면에서 제공하는 인증 타입
+///
+/// * [code] = 제공사 영어 이름 맨 앞자리
+/// * [name] = 제공사 한국어 이름
+/// * [iconPath] = 제공사 로고 아이콘
+/// * [bgColor] = 제공사 로그인 버튼 배경 색
+/// * [fontColor] = 제공사 로그인 버튼 폰트 색
+///
+/// author [Eogeum@naver.com]
 
-  const AuthType(
-      this.code, this.name, this.iconPath, this.bgColor, this.fontColor);
+enum AuthType {
+  google('G', '구글', AppIcons.imgLoginLogoGoogle, Color(0xFFFFFFFF), Color(0xFF000000)),
+  naver('N', '네이버', AppIcons.imgLoginLogoNaver, Color(0xFF03C75A), Color(0xFFFFFFFF)),
+  kakao('K', '카카오', AppIcons.imgLoginLogoKakao, Color(0xFFFEE500), Color(0xDB000000)),
+  email('E', '이메일', '', Color(0xFFBBBBBB), Color(0xFF000000));
+
+  const AuthType(this.code, this.name, this.iconPath, this.bgColor, this.fontColor);
 
   final String code;
   final String name;
