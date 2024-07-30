b81200359c38b5fd5d0fa066ea05d5e4cb6ab32b
Eogeum <Eogeum@naver.com>
2024-07-30 16:32:53 +0900

    [최정창][KKULTRIP-75] 빌드 앱에서도 로그 사용(debug 레벨은 필터링)

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,5 +1,27 @@
+import 'package:flutter_config/flutter_config.dart';
 import 'package:logger/logger.dart';
 
-class CustomLogger{
-  static final Logger logger = Logger();
+class CustomLogger {
+  static final Logger logger = Logger(filter: CustomLogFilter());
+}
+
+class CustomLogFilter extends LogFilter {
+  static const bool _isLogEnabled = true;
+  final bool _hideDebugLog = (FlutterConfig.get('BUILD_TYPE') ?? '') == 'release';
+
+  @override
+  bool shouldLog(LogEvent event) {
+    if (!_isLogEnabled) {
+      return false;
+    }
+
+    var shouldLog = false;
+    if (_hideDebugLog && event.level.value == Level.debug) {
+      shouldLog = false;
+    } else if (event.level.value >= level!.value) {
+      shouldLog = true;
+    }
+
+    return shouldLog;
+  }
 }
