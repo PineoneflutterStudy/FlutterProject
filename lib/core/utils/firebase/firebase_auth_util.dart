6a82172dd894f2337d938496097436f7cce14b28
Eogeum <Eogeum@naver.com>
2024-08-19 12:43:08 +0900

    [최정창][KKULTRIP-30] 로그인 bloc 내 로그인 관련 함수 FirebaseAuthUtil로 이전 및 리펙토링

--- lib/core/utils/firebase/firebase_auth_util.dart
+++ lib/core/utils/firebase/firebase_auth_util.dart
@@ -1,4 +1,14 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
+import 'package:url_launcher/url_launcher.dart';
+
+import '../logger.dart';
 
 class FirebaseAuthUtil {
   final FirebaseAuth auth;
@@ -10,4 +20,120 @@ class FirebaseAuthUtil {
   String? getUserId() => getCurrentUser()?.uid;
 
   bool isLoggedIn() => (getCurrentUser() != null);
+
+  /// ##구글 로그인을 실핸한다.
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? account = await GoogleSignIn().signIn();
+
+      if (account == null) {
+        throw Exception('Google sign-in failed: account == NULL');
+      }
+
+      // 웹인 경우 scope 접근 확인 필요
+      // if (kIsWeb) {
+      //   isAuthorized = await _googleSignIn.canAccessScopes(scopes);
+      // }
+
+      // 구글 인증 정보 가져오기
+      final GoogleSignInAuthentication authentication = await account.authentication;
+
+      // 인증 정보로 인증서 생성
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+          idToken: authentication.idToken, accessToken: authentication.accessToken);
+
+      // 인증서로 파이어베이스 로그인
+      auth.signInWithCredential(credential);
+    } catch (error) {
+      throw Exception('Google sign-in failed: error = $error');
+    }
+  }
+
+  /// ##네이버 로그인을 실핸한다.
+  Future<void> signInWithNaver() async {
+    final String clientId = FlutterConfig.get('NAVER_CLIENT_ID');
+    final String redirectUri =
+        'https://us-central1-gaekkul-project.cloudfunctions.net/naverLoginCallback';
+    // state is random nonce used for security
+    final String state = base64Url.encode(List<int>.generate(16, (_) => Random().nextInt(255)));
+
+    final Uri url = Uri.parse(
+        'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&state=$state');
+    await launchUrl(url);
+  }
+
+  /// ##네이버 로그인과 관련된 앱 링크를 처리한다.
+  Future<void> handleNaverAppLinks(Uri uri) async {
+    try {
+      if (uri.authority == 'login-callback') {
+        // 구글 클라우드에서 생성한 커스텀 토큰 가져오기
+        final String firebaseToken = uri.queryParameters['firebaseToken'] ?? '';
+        if (firebaseToken.isEmpty) {
+          throw Exception('Naver sign-in failed: firebaseToken.isEmpty == TRUE');
+        }
+
+        // 커스텀 토큰으로 파이어베이스 로그인
+        await FirebaseAuth.instance.signInWithCustomToken(firebaseToken);
+      }
+    } catch (error) {
+      throw Exception('Naver sign-in failed: error = $error');
+    }
+  }
+
+  /// ##카카오 로그인을 실핸한다.
+  Future<void> signInWithKakao() async {
+    if (await kakao.isKakaoTalkInstalled()) {
+      // 카카오톡 실행 가능한 경우 카카오톡으로 로그인
+      try {
+        final kakao.OAuthToken authToken = await kakao.UserApi.instance.loginWithKakaoTalk();
+        _onKakaoLoginSucceeded(authToken);
+      } catch (error) {
+        const String _tag = '[Login]';
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오 계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException &&
+            (error.code == 'CANCELED' || error.code == 'access_denied')) {
+          CustomLogger.logger.i('$_tag KakaoTalk sign-in canceled.');
+          return;
+        }
+
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        CustomLogger.logger.w('$_tag KakaoTalk sign-in failed: error = $error');
+        await _signInWithKakaoAccount();
+      }
+    } else {
+      // 카카오톡 실행 불가한 경우 카카오 계정으로 로그인
+      await _signInWithKakaoAccount();
+    }
+  }
+
+  Future<void> _signInWithKakaoAccount() async {
+    try {
+      final kakao.OAuthToken authToken = await kakao.UserApi.instance.loginWithKakaoAccount();
+      _onKakaoLoginSucceeded(authToken);
+    } catch (error) {
+      throw Exception('Kakao account sign-in failed: error = $error');
+    }
+  }
+
+  /// 카카오 비즈앱 심사를 통과해야 카카오 계정 이메일 가져올 수 있다.
+  final bool isKakaoBizApp = false; // fixme 비즈앱 심사 통과 시 변경
+
+  /// 카카오 로그인 성공 후 파이어베이스 로그인
+  void _onKakaoLoginSucceeded(kakao.OAuthToken authToken) {
+    if (isKakaoBizApp) {
+      // 인증 정보로 인증서 생성
+      final OAuthProvider provider = OAuthProvider('oidc.kakao');
+      final OAuthCredential credential =
+          provider.credential(accessToken: authToken.accessToken, idToken: authToken.idToken);
+
+      // 인증서로 파이어베이스 로그인
+      FirebaseAuth.instance.signInWithCredential(credential);
+    } else {
+      // 비즈앱 심사를 통과하지 않아 이메일을 가져올 수 없으므로
+      //eff 이메일 가입 화면으로 이동하는 등 시나리오 필요
+      // add(LoginEvent.userInfoMissing());
+    }
+  }
 }
--- lib/presentation/pages/login/bloc/login_bloc.dart
+++ lib/presentation/pages/login/bloc/login_bloc.dart
@@ -1,22 +1,16 @@
-import 'dart:convert';
-import 'dart:math';
-
 import 'package:app_links/app_links.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_config/flutter_config.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../core/utils/firebase/firebase_auth_util.dart';
 import '../../../../core/utils/logger.dart';
 import '../../../../domain/model/display/login/auth_type.dart';
 
 part 'login_bloc.freezed.dart';
+
 part 'login_event.dart';
+
 part 'login_state.dart';
 
 const String _tag = '[Login]';
@@ -51,14 +45,31 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     FirebaseAuthUtil().auth.userChanges().listen((user) => add(LoginEvent.userChanged(user)));
   }
 
-  void _onLoginOptionItemPressed(Emitter<LoginState> emit, AuthType authType) {
+  Future<void> _onLoginOptionItemPressed(Emitter<LoginState> emit, AuthType authType) async {
+    final FirebaseAuthUtil authUtil = FirebaseAuthUtil();
+
     switch (authType) {
       case AuthType.google:
-        _launchGoogleLogin();
+        try {
+          await authUtil.signInWithGoogle();
+          CustomLogger.logger.i('$_tag Google sign-in succeeded.');
+        } catch (error) {
+          CustomLogger.logger.e('$_tag `Error - $error');
+          //eff 예외처리 필요
+        }
+
       case AuthType.naver:
-        _launchNaverLogin();
+        await authUtil.signInWithNaver();
+
       case AuthType.kakao:
-        _launchKakaoLogin();
+        try {
+          await authUtil.signInWithKakao();
+          CustomLogger.logger.i('$_tag Kakao sign-in succeeded.');
+        } catch (error) {
+          CustomLogger.logger.e('$_tag `Error - $error');
+          //eff 예외처리 필요
+        }
+
       case AuthType.email:
         _launchSignInPage();
     }
@@ -68,7 +79,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     // user 가 null 이라는 건 로그인 된 상태에서 로그아웃 되었다는 건데
     // 정상적인 상황은 아니므로 로그아웃하고 토스트 노출
     if (user == null) {
-      CustomLogger.logger.e('$_tag Error - User changed but is null.');
+      CustomLogger.logger.e('$_tag `Error - User changed but is null.');
       FirebaseAuthUtil().auth.signOut();
       emit(LoginState.loggedOut());
       return;
@@ -85,137 +96,31 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     emit(LoginState.loggedIn());
   }
 
-  Future<void> _launchGoogleLogin() async {
-    try {
-      final GoogleSignInAccount? account = await GoogleSignIn().signIn();
-
-      if (account == null) {
-        CustomLogger.logger.e('$_tag Error - Google login failed. account == NULL');
-        return;
-      }
-
-      // 웹인 경우 scope 접근 확인 필요
-      // if (kIsWeb) {
-      //   isAuthorized = await _googleSignIn.canAccessScopes(scopes);
-      // }
-
-      // 구글 인증 정보 가져오기
-      final GoogleSignInAuthentication authentication = await account.authentication;
-      CustomLogger.logger.i('$_tag Google login successed.');
-
-      // 인증 정보로 인증서 생성
-      final OAuthCredential credential = GoogleAuthProvider.credential(
-          idToken: authentication.idToken, accessToken: authentication.accessToken);
-
-      // 인증서로 파이어베이스 로그인
-      FirebaseAuth.instance.signInWithCredential(credential);
-    } catch (error) {
-      CustomLogger.logger.e('$_tag Google login failed. error = $error');
-    }
-  }
-
   /// ## 네이버 로그인 결과를 받기 위한 앱 링크 초기화
-  /// [launchNaverLogin]의 결과를 받기 위해선 반드시 호출하는 위젯의 [StatefulWidget.initState]에서 호출이 필요하다.
+  /// [signInWithNaver]의 결과를 받기 위해선 호출이 필요하다.
   Future<void> _initAppLinks() async {
+    final FirebaseAuthUtil authUtil = FirebaseAuthUtil();
+
     final AppLinks appLinks = AppLinks();
     final Uri? initialLink = await appLinks.getInitialLink();
     if (initialLink != null) {
-      _onReceiveAppLinks(initialLink);
+      authUtil.handleNaverAppLinks(initialLink);
     }
 
     appLinks.uriLinkStream.listen((uri) {
-      _onReceiveAppLinks(uri);
-    }, onError: (a, b) {
-      CustomLogger.logger.e('$_tag Error - Naver login failed. cause = $a / $b');
-    });
-  }
+      CustomLogger.logger.d('$_tag App links received. uri = ${uri.toString()}');
 
-  Future<void> _onReceiveAppLinks(Uri uri) async {
-    CustomLogger.logger.i('$_tag App links received. uri = ${uri.toString()}');
-
-    if (uri.authority == 'login-callback') {
-      // 구글 클라우드에서 생성한 커스텀 토큰 가져오기
-      final String firebaseToken = uri.queryParameters['firebaseToken'] ?? '';
-      if (firebaseToken.isEmpty) {
-        CustomLogger.logger.e('$_tag Error - Naver login failed. firebaseToken.isEmpty == TRUE');
-        return;
-      }
-
-      CustomLogger.logger.i('$_tag Naver login successed.');
-
-      // 커스텀 토큰으로 파이어베이스 로그인
-      await FirebaseAuth.instance.signInWithCustomToken(firebaseToken);
-    }
-  }
-
-  Future<void> _launchNaverLogin() async {
-    CustomLogger.logger.i('$_tag launchNaverLogin()');
-
-    final String clientId = FlutterConfig.get('NAVER_CLIENT_ID');
-    final String redirectUri =
-        'https://us-central1-gaekkul-project.cloudfunctions.net/naverLoginCallback';
-    // state is random nonce used for security
-    final String state = base64Url.encode(List<int>.generate(16, (_) => Random().nextInt(255)));
-
-    final Uri url = Uri.parse(
-        'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&state=$state');
-    await launchUrl(url);
-  }
-
-  Future<void> _launchKakaoLogin() async {
-    // 카카오톡 실행 가능 여부 확인
-    // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-    if (await kakao.isKakaoTalkInstalled()) {
       try {
-        final kakao.OAuthToken authToken = await kakao.UserApi.instance.loginWithKakaoTalk();
-        CustomLogger.logger.i('$_tag KakaoTalk login successed.');
-        _onKakaoLoginSuccessed(authToken);
+        authUtil.handleNaverAppLinks(uri);
       } catch (error) {
-        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-        if (error is PlatformException &&
-            (error.code == 'CANCELED' || error.code == 'access_denied')) {
-          CustomLogger.logger.i('$_tag KakaoTalk login canceled.');
-          return;
-        }
-
-        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-        CustomLogger.logger.e('$_tag Error - KakaoTalk login failed. error = $error');
-        await _loginWithKakaoAccount();
+        CustomLogger.logger.e('$_tag `Error - $error');
+        //eff 예외처리 필요
       }
-    } else {
-      await _loginWithKakaoAccount();
-    }
-  }
-
-  Future<void> _loginWithKakaoAccount() async {
-    try {
-      final kakao.OAuthToken authToken = await kakao.UserApi.instance.loginWithKakaoAccount();
-      CustomLogger.logger.i('$_tag KakaoAccount login successed.');
-      _onKakaoLoginSuccessed(authToken);
-    } catch (error) {
-      CustomLogger.logger.e('$_tag KakaoAccount login failed. error = $error');
-    }
-  }
-
-  /// 카카오 비즈앱 심사를 통과해야 카카오 계정 이메일 가져올 수 있다.
-  final bool isKakaoBizApp = false; // fixme 비즈앱 심사 통과 시 변경
-
-  /// 카카오 로그인 성공 후 파이어베이스 로그인
-  void _onKakaoLoginSuccessed(kakao.OAuthToken authToken) {
-    if(isKakaoBizApp) {
-      // 인증 정보로 인증서 생성
-      final OAuthProvider provider = OAuthProvider('oidc.kakao');
-      final OAuthCredential credential =
-      provider.credential(accessToken: authToken.accessToken, idToken: authToken.idToken);
-
-      // 인증서로 파이어베이스 로그인
-      FirebaseAuth.instance.signInWithCredential(credential);
-    } else {
-      // 비즈앱 심사를 통과하지 않아 이메일을 가져올 수 없으므로
-      //eff 이메일 가입 화면으로 이동하는 등 시나리오 필요
-      // add(LoginEvent.userInfoMissing());
-    }
+    }, onError: (error, stackTrace) {
+      CustomLogger.logger
+          .e('`Error - Naver sign-in failed: error = $error, stackTrace = $stackTrace');
+      //eff 예외처리 필요
+    });
   }
 
   void _launchSignInPage() {
