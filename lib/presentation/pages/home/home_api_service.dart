250591b97699977199e888f1d9fb99fbd9709afb
Meconic <min9709@pineone.com>
2024-08-30 15:07:29 +0900

    [박현민][KKULTRIP-66] 크롤링 HardCoding으로 대체

--- lib/presentation/pages/home/home_api_service.dart
+++ lib/presentation/pages/home/home_api_service.dart
@@ -5,15 +5,14 @@ import 'package:http/http.dart' as http;
 import 'home_model.dart';
 
 class HomeApiService {
-  static final String indexUrl =
-      'https://www.yeogi.com/_next/data/jzaVnKYL2Uswa4LDBlzpL/index.json';
-  // https://www.yeogi.com/_next/data/jzaVnKYL2Uswa4LDBlzpL/index.json
-  //'https://www.yeogi.com/_next/data/MuNcWaI6D16ttbvQoSWZ2/index.json';
+  // static final String indexUrl =
+  //     'https://www.yeogi.com/_next/data/PkbqwxduEf53Qt2MewwBK/index.json';
 
   static Future<HomeModel> getIndex() async {
-    final url = Uri.parse(indexUrl);
-    final response = await http.get(url);
-
-    return HomeModel.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+    // final url = Uri.parse(indexUrl);
+    // final response = await http.get(url);
+    //
+    // return HomeModel.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+    return HomeModel.hardJson(); // http을 하드 코딩으로 변경
   }
 }
--- lib/presentation/pages/home/home_model.dart
+++ lib/presentation/pages/home/home_model.dart
@@ -13,10 +13,39 @@ enum Groups {
 class HomeModel {
   final List<String> titleList, imageList, locationList;
 
+  // 크롤링
   HomeModel.fromJson(dynamic json) // state는 추후 State로 변경
       : titleList = crawling(json, Groups.title),
         imageList = crawling(json, Groups.image),
         locationList = crawling(json, Groups.location);
+
+  // 하드코딩
+  HomeModel.hardJson()
+      : titleList = ["국내 인기 여행지"],
+        imageList = [
+          "https://image.goodchoice.kr/exhibition/cms/Region_jeju_03_20231103163922.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Seoul_03_20231103163936.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Busan_03_20231103163951.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Gangneung_03_20231103164004.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Incheon_03_20231103164014.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Gyeongju_03_20231103164027.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region__Haeundae_03_20231103164044.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region__Gapyeong_03_20231103164056.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Yeo_su_03_20231103164109.png",
+          "https://image.goodchoice.kr/exhibition/cms/Region_Sokcho_02_20231117140152.png"
+        ],
+        locationList = [
+          "제주도",
+          "서울",
+          "부산",
+          "강릉",
+          "인천",
+          "경주",
+          "해운대",
+          "가평",
+          "여수",
+          "속초"
+        ];
 
   static List<String> crawling(dynamic json, Groups groups) {
     List<String> result = [];
