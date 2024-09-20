d1640888efebbafb90340d4793a2cb9a5e37187c
Nanioi <0802skdus@naver.com>
2024-09-21 01:27:42 +0900

    [곽나연][KKULTRIP-109] 공통 로딩 이미지 사이즈 조절가능하도록 개선

--- lib/presentation/main/common/component/widget/mangmung_loding_indicator.dart
+++ lib/presentation/main/common/component/widget/mangmung_loding_indicator.dart
@@ -4,14 +4,17 @@ import 'package:lottie/lottie.dart';
 import '../../../../../core/theme/constant/app_icons.dart';
 
 class MangmungLoadingIndicator extends StatelessWidget {
-  const MangmungLoadingIndicator({super.key});
+  final double size;
+  const MangmungLoadingIndicator({super.key, this.size = 150});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      child: Center(child: LottieBuilder.asset(AppIcons.mangmungLoadingLottie, width: 150, height: 150,)),
+      child: Center(
+        child: LottieBuilder.asset(AppIcons.mangmungLoadingLottie, width: size, height: size,),
+      ),
     );
   }
 }
