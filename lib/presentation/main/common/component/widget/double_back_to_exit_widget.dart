08e98ec30a29a9a69b1ee278bcb94901ebb077a8
Eogeum <Eogeum@naver.com>
2024-09-24 11:10:24 +0900

    [최정창][KKULTRIP-29] 종료 안내 위젯 추가하고 PopScope 동작 안하는 이슈로 go_router 라이브러리 다운그레이드(14.2.0 -> 12.1.3)

---
+++ lib/presentation/main/common/component/widget/double_back_to_exit_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../../../core/utils/common_utils.dart';
+import '../../../../../core/utils/logger.dart';
+
+/// ## 종료 안내 위젯
+/// 2초 내 뒤로 가기 두 번 입력해야 종료되도록 적용된 위젯
+///
+/// author [Eogeum@naver.com]
+class DoubleBackToExitWidget extends StatefulWidget {
+  final Widget child;
+
+  const DoubleBackToExitWidget({required this.child});
+
+  @override
+  State<StatefulWidget> createState() => _DoubleBackToExitState();
+}
+
+class _DoubleBackToExitState extends State<DoubleBackToExitWidget> {
+  DateTime? _lastPressedTime;
+  bool _canPop = false;
+
+  @override
+  Widget build(BuildContext context) => PopScope(
+        child: widget.child,
+        canPop: _canPop,
+        onPopInvokedWithResult: (didPop, result) {
+          if (didPop) {
+            return;
+          }
+
+          final DateTime now = DateTime.now();
+          if (_lastPressedTime == null || now.difference(_lastPressedTime!) > Duration(seconds: 2)) {
+            _lastPressedTime = now;
+            CommonUtils.showToastMsg('앱을 종료하려면 한 번 더 눌러주세요.');
+            setState(() => _canPop = false);
+            return;
+          } else {
+            setState(() => _canPop = true);
+          }
+        },
+      );
+}
