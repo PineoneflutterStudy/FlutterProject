26173d4df6e03fc7383baeb76449792ad45a6b2d
김경태 <kim3jjj@naver.com>
2024-09-24 15:00:28 +0900

    [김경태][KKULTRIP-113] 1. 변경된 복수 마커 이미지 적용 및 이미지에 맞는 Line Color 적용 1) 공용 커스텀 floatingActionButton 으로 변경

--- lib/presentation/pages/meet/widgets/save_meet_widget.dart
+++ lib/presentation/pages/meet/widgets/save_meet_widget.dart
@@ -4,14 +4,16 @@ import 'package:logger/logger.dart';
 
 import '../../../../core/theme/constant/app_colors.dart';
 import '../../../../core/utils/logger.dart';
+import '../../../../domain/model/display/common/fab_item.dart';
 import '../../../../domain/model/display/meet/location_db.model.dart';
+import '../../../main/common/component/widget/common_fab_widget.dart';
 import 'save_meet_list_item_widget.dart';
 
 final Logger _logger = CustomLogger.logger;
 class SaveMeetWidget extends StatelessWidget {
   final List<LocationDbModel> locationsInfo;
-  final void Function()? onAddMeetLocation; // + 약속 추가하기 버튼 입력
-  final void Function()? onBtnAllDelete; // delete 모두 삭제 버튼
+  final VoidCallback onAddMeetLocation; // + 약속 추가하기 버튼 입력
+  final VoidCallback onBtnAllDelete; // delete 모두 삭제 버튼
   const SaveMeetWidget ({
     super.key,
     required this.locationsInfo,
@@ -30,67 +32,26 @@ class SaveMeetWidget extends StatelessWidget {
             return SaveMeetListItemWidget(locationModel: locationsInfo[index]);
           },
         ),
-        Positioned(
-          bottom: 16.0, // 하단에서의 거리
-          right: 16.0, // 우측에서의 거리
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white, // 배경색 (필요에 따라 조정)
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 4.0,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            child: SpeedDial(
-              animatedIcon: AnimatedIcons.menu_close,
-              visible: true,
-              curve: Curves.bounceIn,
-              backgroundColor: AppColors.primary,
-              children: [
-                SpeedDialChild(
-                    child: const Icon(Icons.add, color: AppColors.onPrimary),
-                    label: "약속 추가하기",
-                    labelStyle: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.blackPolyLine80,
-                        fontSize: 13.0),
-                    backgroundColor: AppColors.primary,
-                    labelBackgroundColor: AppColors.primary,
-                    onTap: onAddMeetLocation),
-                SpeedDialChild(
-                  child: const Icon(
-                    Icons.delete_forever,
-                    color: AppColors.onPrimary,
-                  ),
-                  label: "모두 삭제",
-                  backgroundColor: AppColors.primary,
-                  labelBackgroundColor: AppColors.primary,
-                  labelStyle: const TextStyle(
-                      fontWeight: FontWeight.w500, color: AppColors.blackPolyLine80, fontSize: 13.0),
-                  onTap: onBtnAllDelete,
-                )
-                ,SpeedDialChild(
-                  child: const Icon(
-                    Icons.delete,
-                    color: AppColors.onPrimary,
-                  ),
-                  label: "삭제",
-                  backgroundColor: AppColors.primary,
-                  labelBackgroundColor: AppColors.primary,
-                  labelStyle: const TextStyle(
-                      fontWeight: FontWeight.w500, color: AppColors.blackPolyLine80, fontSize: 13.0),
-                  onTap: () {
-                    // 모두 삭제 확인 Dialog 출력 후 모두 삭제 진행
-                  },
-                )
-              ],
-            ),
-          ),
-        ),
+        CommonFabWidget(
+          mainIcon: Icons.edit_note_rounded,
+          padding: 20.0,
+          fabItems: [
+            FabItem(
+                icon: Icons.add,
+                label: '약속 추가',
+                onTap: onAddMeetLocation),
+            FabItem(
+                icon: Icons.delete_forever,
+                label: '삭제',
+                onTap: () {
+
+                }),
+            FabItem(
+                icon: Icons.delete_forever,
+                label: '모두 삭제',
+                onTap: onBtnAllDelete),
+          ],
+        )
       ],
     );
   }
